96cca42795a5a873ddbd8335c8b6e4fc4c29428e
Ana Chatillon <a_chantil@hotmail.com>
2025-04-04 02:50:41 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,55 @@
+let paris = {
+  name: "Paris",
+  country: "France",
+  language: "French",
+  temperature: 18,
+  capitalCity: true,
+};
+
+console.log(paris.temperature);
+
+let sydney = {
+  name: "Sydney",
+  country: "Australia",
+  language: "English",
+  temperature: 25,
+  capitalCity: false,
+};
+
+console.log(sydney.temperature);
+
+let australianCities = ["Perth", "Melbourne", "Adelaide"];
+console.log(australianCities[0]);
+console.log(australianCities[1]);
+console.log(australianCities[2]);
+
+let cities = [
+  {
+    name: "Lisbon",
+    country: "Portugal",
+    language: "Portuguese",
+    temperature: 26,
+    capitalCity: true,
+  },
+  {
+    name: "Madrid",
+    country: "Spain",
+    language: "Spanish",
+    temperature: 24,
+    capitalCity: true,
+  },
+  {
+    name: "Athens",
+    country: "Greece",
+    language: "Greek",
+    temperature: 28,
+    capitalCity: true,
+  },
+];
+
+console.log(cities[0].name);
+console.log(cities[0].temperature);
+console.log(cities[1].name);
+console.log(cities[1].temperature);
+console.log(cities[2].name);
+console.log(cities[2].temperature);
